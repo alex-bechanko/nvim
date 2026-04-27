93054daa70559477f7cb3852c958c12b0b752fed
Alex Bechanko <alexbechanko@gmail.com>
2026-04-27 09:41:59 -0400

    add shortcuts for copy/paste from system clipboard

--- init.lua
+++ init.lua
@@ -138,3 +138,10 @@ vim.keymap.set('n', 'zM', require('ufo').closeAllFolds, { desc = 'Close all fold
 vim.keymap.set('n', '<leader>?', function()
 	require('which-key').show({ global = false })
 end, { desc = 'Display Buffer Local Keymaps' })
+
+-- keymaps for copying to from system clipboard
+vim.keymap.set({ 'n', 'v' }, '<leader>y', '"+y', { desc = 'Copy to system clipboard' })
+vim.keymap.set('n', '<leader>Y', '"+y$', { desc = 'Copy line to system clipboard' })
+-- keymaps for pasting from system clipboard
+vim.keymap.set({ 'n', 'v' }, '<leader>p', '"+p', { desc = 'Paste from system clipboard AFTER cursor' })
+vim.keymap.set({ 'n', 'v' }, '<leader>P', '"+P', { desc = 'Paste from system clipboard BEFORE cursor' })
